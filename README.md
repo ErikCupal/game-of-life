5b148485fa54289d6e43e7b78409b14060c8caad
Erik Cupal <erik@cup.al>
2018-02-12 20:25:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Game of life
+
+Simple implementation of Game of life.
+
+![demo](demo.png)
+
+## How to run
+
+0. [Install stack](https://docs.haskellstack.org/en/stable/README/#how-to-install) if you don't have it installed
+0. `git clone https://github.com/ErikCupal/game-of-life.git`
+0. `cd game-of-life`
+0. `stack setup`
+0. `stack runghc -- src/Main.hs`
+0. type location of a pattern, eg. `seeds/pulsar.txt`
---
+++ src/Main.hs
@@ -0,0 +1,121 @@
+module Main where
+
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+import Data.Foldable (toList)
+import Data.List (intercalate)
+import qualified Data.Sequence as Seq
+import Data.Sequence (Seq, fromList, mapWithIndex)
+import System.IO (IOMode(..), getLine, hGetContents, openFile)
+
+type Board = Seq Row
+
+type Row = Seq Cell
+
+data Cell
+  = Alive
+  | Dead
+
+type Position = (Int, Int)
+
+-- IO
+main :: IO ()
+main = do
+  putStr "Enter path of file with pattern: "
+  filePath <- getLine
+  file <- openFile filePath ReadMode
+  content <- hGetContents file
+  let board = parseBoard content
+  printWorldLoop board 1
+
+printWorldLoop :: Board -> Int -> IO ()
+printWorldLoop board generation = do
+  moveToTop
+  clearScreen
+  putStrLn $ "Generation: " ++ show generation ++ "\n"
+  let nextBoard = nextWorld board
+  putStrLn . showBoard $ nextBoard
+  threadDelay 1000000 -- wait 1 second
+  printWorldLoop nextBoard (generation + 1)
+
+moveToTop :: IO ()
+moveToTop = putStr "\ESC[H"
+
+clearScreen :: IO ()
+clearScreen = putStr "\ESC[2J"
+
+-- Parsing
+parseBoard :: String -> Board
+parseBoard s =
+  if length nums < 2
+    then Seq.empty
+    else board
+  where
+    nums = fmap read (words s) :: [Int]
+    width:height:rawBoard = nums
+    rows = splitEvery width rawBoard
+    board = numsToBoard rows
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n list = first : splitEvery n rest
+  where
+    (first, rest) = splitAt n list
+
+showBoard :: Board -> String
+showBoard = intercalate "\n" . toList . fmap showRow
+  where
+    showRow :: Row -> String
+    showRow = unwords . toList . fmap showCell
+
+showCell :: Cell -> String
+showCell Alive = "O"
+showCell Dead = " "
+
+numsToBoard :: [[Int]] -> Board
+numsToBoard = fromList . fmap (fromList . fmap numToCell)
+
+numToCell :: Int -> Cell
+numToCell 1 = Alive
+numToCell _ = Dead
+
+-- Next world logic
+nextWorld :: Board -> Board
+nextWorld board
+  | height == 0 || width == 0 = board
+  | otherwise = mapWithIndex mapRow board
+  where
+    height = Seq.length board
+    width = Seq.length firstRow
+    firstRow = Seq.index board 0
+    mapRow x = mapWithIndex (\y cell -> mapCell (x, y) cell)
+    mapCell (x, y) cell = nextCell cell (x, y) board
+
+nextCell :: Cell -> Position -> Board -> Cell
+nextCell cell position board = nextCell' cell (neighboursAlive position board)
+  where
+    nextCell' :: Cell -> Int -> Cell
+    -- get next cell by providing a cell and count of neighbours alive
+    nextCell' Dead 3 = Alive
+    nextCell' Alive 2 = Alive
+    nextCell' Alive 3 = Alive
+    nextCell' _ _ = Dead
+
+neighboursAlive :: Position -> Board -> Int
+neighboursAlive (x, y) board =
+  length . filter id . fmap neighbourAlive $ relativeNeighbourPositions
+  where
+    neighbourAlive :: Position -> Bool
+    neighbourAlive (nx, ny) = cellAlive (x + nx, y + ny) board
+
+cellAlive :: Position -> Board -> Bool
+cellAlive (x, y) board =
+  case Seq.lookup x board of
+    Just row ->
+      case Seq.lookup y row of
+        Just Alive -> True
+        _ -> False
+    _ -> False
+
+relativeNeighbourPositions =
+  [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
